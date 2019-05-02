cfb18703fe4ab8593b9505b354f6cfea88904c8b
cterrasid <clarette.terrasi@gmail.com>
2019-05-02 23:16:31 +0200

    Improve handler to flip cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,7 +10,7 @@ const imgUrl = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 const apiUrl = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
 
 //Funcion para crear elementos
-function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, clasS, ul) {
+function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, class1, class2, ul) {
   //Creo elementos
   const liEl = document.createElement(li);
   const imgPoke = document.createElement(img1);
@@ -18,8 +18,10 @@ function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, clas
   //Añado atributos y clases
   imgPoke.setAttribute(src, imgUrl1);
   imgPoke.setAttribute(id, idNumber);
-  imgPoke.classList.add(clasS);
+  imgPoke.classList.add(class1);
   imgAdalab.setAttribute(src, imgUrl2);
+  imgAdalab.setAttribute(id, idNumber);
+  imgAdalab.classList.add(class2);
   //incorporo los elementos donde corresponde
   liEl.appendChild(imgPoke);
   liEl.appendChild(imgAdalab);
@@ -42,6 +44,7 @@ function handleRadioBox(e) {
   for (const radioBox of radioBoxEl) {
     (radioBox.id !== radioBoxValue) ? radioBox.checked = false : radioBox.checked;
   }
+  //Condicion para añadir mas cartas si hago otra seleccion
 }
 
 //Escucho el click del boton "Comenzar"
@@ -50,26 +53,41 @@ btnEl.addEventListener('click', handleBtnClick);
 function handleBtnClick() {
   //realizo la petición al servidor
   fetch(`${apiUrl}${radioBoxValue}.json`)
-  //Que me responderá en un archivo .json
+    //Que me responderá en un archivo .json
     .then(response => response.json())
-  //La respuesta en sí, es la siguiente:
+    //La respuesta en sí, es la siguiente:
     .then(result => {
       //recorro los elementos de la API, que son un array de objetos
       for (const data of result) {
         const dataImgUrl = data.image;
         const dataId = data.pair;
         //creo los elementos de la API que me interesan
-        const paintEl = paintElements('li', 'img', 'img', 'src', dataImgUrl, imgUrl, 'id', dataId, 'hidden', ulCardsEl);
+        const paintEl = paintElements('li', 'img', 'img', 'src', dataImgUrl, imgUrl, 'id', dataId, 'hide', 'show', ulCardsEl);
 
         //los escucho para poder manejarlos
-        paintEl.addEventListener('click', handleCardClick);
+        console.log(paintEl.imgAdalab);
+        
+        //paintEl.addEventListener('click', handleCardClick);
       }
     });
 }
 
 function handleCardClick(e) {
-  e.currentTarget.classList.remove('hidden');
-}
-// const addRemoveClass = (classA, classB, el) => { el.classList.add(classA); el.classList.remove(classB); };
+  const getId = e.currentTarget.getAttribute('id');
 
-handleCardClick();
+  if (getId.style.display === 'none') {
+    getId.style.display = 'block';
+  } else {
+    getId.style.display = 'none';
+  }
+}
+
+// //funcion para obtener elementos y cambiar clases
+// function flipCards(el, atr, clasS) {
+//   //obtengo elementos segun un atributo (id)
+//   const getId = el.getAttribute(atr);
+//   const getClass = el.getAttribute(atr);
+//   //añado/remuevo clases
+//     getId.classList.add(clasS);
+  
+// }
