25cef73c2cf49ee751c03ab3a9d3201baa1a59b5
cterrasid <clarette.terrasi@gmail.com>
2019-05-02 03:18:12 +0200

    Create styles

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,16 +10,22 @@ const imgUrl = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 const apiUrl = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
 
 //Funcion para crear elementos
-function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, ul) {
+function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, clasS, ul) {
+  //Creo elementos
   const liEl = document.createElement(li);
   const imgPoke = document.createElement(img1);
   const imgAdalab = document.createElement(img2);
+  //Añado atributos y clases
   imgPoke.setAttribute(src, imgUrl1);
   imgPoke.setAttribute(id, idNumber);
+  imgPoke.classList.add(clasS);
   imgAdalab.setAttribute(src, imgUrl2);
+  //incorporo los elementos donde corresponde
   liEl.appendChild(imgPoke);
   liEl.appendChild(imgAdalab);
 
+  liEl.addEventListener('click', handleCardClick);
+  //devuelve los elementos dentro de mi ul
   return ul.appendChild(liEl);
 }
 
@@ -37,7 +43,6 @@ function handleRadioBox(e) {
   }
 }
 
-
 //Escucho el click del boton "Comenzar"
 btnEl.addEventListener('click', handleBtnClick);
 //Cuando haga click en "Comenzar":
@@ -49,12 +54,19 @@ function handleBtnClick() {
   //La respuesta en sí, es la siguiente:
     .then(result => {
       //recorro los elementos de la API, que son un array de objetos
-      //FOR-OF
       for (const data of result) {
         const dataImgUrl = data.image;
         const dataId = data.pair;
 
-        paintElements('li', 'img', 'img', 'src', dataImgUrl, imgUrl, 'id', dataId, ulCardsEl);
+        paintElements('li', 'img', 'img', 'src', dataImgUrl, imgUrl, 'id', dataId, 'hidden', ulCardsEl);
+
       }
     });
 }
+
+function handleCardClick(e) {
+    e.currentTarget.classList.remove('hidden');
+}
+// const addRemoveClass = (classA, classB, el) => { el.classList.add(classA); el.classList.remove(classB); };
+
+handleCardClick();
