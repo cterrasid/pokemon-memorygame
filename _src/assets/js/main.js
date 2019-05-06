2a5025496e45aa05d78f2f8d3d405e45db1e3ee5
cterrasid <clarette.terrasi@gmail.com>
2019-05-06 02:15:16 +0200

    Improve game logic to compare cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,10 +7,10 @@ const ulCardsEl = document.querySelector('.cards__list');
 
 const imgUrl = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 const apiUrl = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
-
 //VARIABLES
 let radioInputChecked = 0;
 let pokeCardsId = 0;
+let originalList = '';
 
 //FUNCION PARA CREAR ELEMENTOS
 function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, class1, class2, ul) {
@@ -32,7 +32,7 @@ function paintElements(li, img1, img2, src, imgUrl1, imgUrl2, id, idNumber, clas
   liEl.addEventListener('click', flipCardWhenClick);
   liEl.addEventListener('click', compareCardsId);
   //escucho a la imagen de pokemon para obtener su id
- // imgPoke.addEventListener('click', compareCardsId);
+  // imgPoke.addEventListener('click', compareCardsId);
   //devuelve los elementos dentro de mi ul
   return ul.appendChild(liEl);
 }
@@ -136,12 +136,41 @@ function shuffle(array) {
   }
   return array;
 }
+//FUNCIONES PARA OCULTAR/MOSTRAR ELEMENTOS
+function showCards(firstEl, secondEl, show, hide) {
+  firstEl.classList.add(show);
+  firstEl.classList.remove(hide);
+  secondEl.classList.add(show);
+  secondEl.classList.remove(hide);
+}
+function hideCards(firstEl, secondEl, show, hide) {
+  firstEl.classList.remove(show);
+  firstEl.classList.add(hide);
+  secondEl.classList.add(hide);
+  secondEl.classList.remove(show);
+}
 //comparo los id
 function compareCardsId(e) {
-  //recojo el id del primer carajito de la lista
-  pokeCardsId = e.currentTarget.firstChild.id;
+  let valueToCompare = e.currentTarget.firstChild.id;
+  const secondPokeCard = e.currentTarget.firstChild;
+  const secondAdaCard = e.currentTarget.lastChild;
+  const firstPokeCard = originalList.firstChild;
+  const firstAdaCard = originalList.lastChild;
+  //si el valor de pokeCardsId es diferente de 0 comparamos con el siguiente id
   if (pokeCardsId !== 0) {
-    const valueToCompare = pokeCardsId;
-    console.log(valueToCompare);
+    //establezco el valor del id dentro de una variable
+    //lo comparo con pokeCardsId
+    if (valueToCompare !== pokeCardsId) {
+    //si son diferentes
+      showCards(firstAdaCard, secondAdaCard, 'show', 'hide');
+      hideCards(firstPokeCard, secondPokeCard, 'show', 'hide');
+      setTimeout(hideCards, 1000);
+    }
+    pokeCardsId = 0; //volvemos a pones 0 como valor por defecto a pokeCardsId para empezar el proceso de nuevo.
+
+  } else { // si el valor de pokeCardsId es igual a 0, asignamos el valor del id de la prmera carta que se voltea
+    pokeCardsId = e.currentTarget.firstChild.id;
+    originalList = e.currentTarget; ///-------- guardamos el liel al que se le dio click para darle la vuelta si es necesario.
   }
+
 }
